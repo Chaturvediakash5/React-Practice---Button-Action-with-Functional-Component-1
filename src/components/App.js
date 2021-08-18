083d59ac941ffaac5592f189d6a6c5b1ba10fc74
aditya-0902 <88245845+aditya-0902@users.noreply.github.com>
2021-08-18 14:52:42 +0530

    problem solved

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,15 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const[phidden,setHidden]=useState(true);
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={()=>{
+        setHidden(!phidden);
+      }}>Click me</button>
+      {phidden?null:
+      <p id="para">Hello, I've learnt to use the full-stack evaluation tool.
+       This makes me so happy</p>}
     </div>
   );
 }
